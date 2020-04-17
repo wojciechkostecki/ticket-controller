616101b097b18402a04b70455f84d8703b03ca80
Wojciech Kostecki <wt.kostecki@gmail.com>
2020-04-17 22:11:33 +0200

    Creating a passengers table and assigning tickets to them

--- src/TicketController.java
+++ src/TicketController.java
@@ -1,3 +1,14 @@
 public class TicketController {
-    
+    public static void main(String[] args) {
+        TicketMachine ticketMachine = new TicketMachine();
+
+        Passenger[] passengers = new Passenger[3];
+        passengers[0] = new Passenger("Wojciech","Kostecki");
+        passengers[1] = new Passenger("Rafał","Bobko");
+        passengers[2] = new Passenger("Damian","Podbilski");
+
+        ticketMachine.buyTicket(passengers[0], 30);
+        ticketMachine.buyTicket(passengers[1], 20);
+        ticketMachine.buyTicket(passengers[2], 15);
+    }
 }
